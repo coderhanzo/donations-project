8a18ad0d9fe8ed5ceadbfdfbd3dfa6a15ce0c1f1
nanakofi1998 <kevinlauren1998@gmail.com>
2024-11-22 13:22:13 -0400

    reverted dashboard to old I will fix it during the weekend

--- frontend/src/app/dashboard/page.js
+++ frontend/src/app/dashboard/page.js
@@ -1,27 +1,23 @@
-'use client';
+'use client'
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation'; // Use correct router import for App Router
-import CustomSidebar from '../components/CustomSidebar';
-import Navbar from './_components/Navbar';
-import Charts from '../components/Charts';
+import CustomSidebar from '../components/CustomSidebar'
+import Navbar from './_components/Navbar'
+import Charts from '../components/Charts'
+
+
 
 const Dashboard = () => {
-  const router = useRouter();
 
-  useEffect(() => {
-    const session = localStorage.getItem('loggedInUser'); // Adjust the key as per your implementation
 
-    if (!session) {
-      router.push('/auth/login'); // Redirect to login page if not authenticated
-    }
-  }, [router]);
+    return (
+        <div className='flex flex-col'>
 
-  return (
-    <div className="flex flex-col">
-      <Charts />
-    </div>
-  );
-};
+            <Charts />
 
-export default Dashboard;
+
+        </div>
+    )
+}
+
+
+export default Dashboard
